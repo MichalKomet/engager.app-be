46347af1e40d941ae95c43b990d73cab1978e557
Michał Gwoździej <gwozdziej.michal@gmail.com>
2025-01-19 17:04:43 +0100

    create main router

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import mainRouter from './src/index.js'
 
 const app = express();
 app.use(express.json());
 
 const SERVER_PORT = process.env.SERVER_PORT;
+
+app.use('/', mainRouter);
 
 app.listen(SERVER_PORT,  () => {
     console.log(`App listening on port ${SERVER_PORT}`);
