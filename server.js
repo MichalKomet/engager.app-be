c92e1d5196a255004fed548df42393805995ebbb
Michał Gwoździej <gwozdziej.michal@gmail.com>
2025-01-21 22:29:29 +0100

    implement cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import mainRouter from './src/index.js'
 import { globalErrorHandler } from './validations/handleValidationErrors.js'
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 
 const SERVER_PORT = process.env.SERVER_PORT;
 
+app.use(cors());
 app.use(mainRouter);
 app.use(globalErrorHandler);
 
